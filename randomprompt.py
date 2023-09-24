6b9641aca7e34b6949ab588bd858a17f8f94c2a3
mickr777 <115216705+mickr777@users.noreply.github.com>
2023-09-24 11:11:54 +1000

    Add local model Dropdown

--- randomprompt.py
+++ randomprompt.py
@@ -1,4 +1,5 @@
-import torch, os
+import torch
+import os
 from typing import Literal, Optional
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from invokeai.app.invocations.baseinvocation import (
@@ -13,16 +14,47 @@ from invokeai.app.invocations.primitives import StringOutput
 CACHE_DIR = "model_cache"
 os.makedirs(CACHE_DIR, exist_ok=True)
 
+
+def list_local_models() -> list:
+    if not os.path.exists(CACHE_DIR):
+        return []
+    # Assuming each model is stored in its own folder within CACHE_DIR
+    models = [
+        d.replace("models", "").replace("-", " ")
+        for d in os.listdir(CACHE_DIR)
+        if os.path.isdir(os.path.join(CACHE_DIR, d))
+    ]
+    return sorted(models, key=lambda x: x.lower())
+
+
+available_models = list_local_models()
+
+if available_models:
+    models_str = ", ".join([repr(m) for m in available_models])
+    ModelLiteral = eval(f'Literal["None", {models_str}]')
+else:
+    ModelLiteral = Literal["None"]
+
+
 def is_model_cached(model_name: str) -> bool:
     """Check if the model and its tokenizer are cached locally in a custom directory."""
     try:
-        AutoTokenizer.from_pretrained(model_name, local_files_only=True, cache_dir=CACHE_DIR)
-        AutoModelForCausalLM.from_pretrained(model_name, local_files_only=True, cache_dir=CACHE_DIR)
+        AutoTokenizer.from_pretrained(
+            model_name, local_files_only=True, cache_dir=CACHE_DIR)
+        AutoModelForCausalLM.from_pretrained(
+            model_name, local_files_only=True, cache_dir=CACHE_DIR)
         return True
     except:
         return False
 
-@invocation("Random_Prompt_Maker_GPT2", title="Random Prompt Maker Using GPT2", tags=["prompt", "gpt2"], category="prompt", version="1.0.0")
+
+@invocation(
+    "Random_Prompt_Maker_GPT2",
+    title="Random Prompt Maker Using GPT2",
+    tags=["prompt", "gpt2"],
+    category="prompt",
+    version="1.0.0",
+)
 class GPT2PromptInvocation(BaseInvocation):
     """Generates a random prompt using GPT-2"""
 
@@ -35,25 +67,21 @@ class GPT2PromptInvocation(BaseInvocation):
         description="Context for the prompt generation",
         ui_component=UIComponent.Textarea,
     )
-    max_length: Optional[int] = InputField(default=100, description="Max length of the generated text")
-    temperature: Optional[float] = InputField(default=0.7, description="Controls the randomness of predictions")
+    max_length: Optional[int] = InputField(
+        default=100, description="Max length of the generated text")
+    temperature: Optional[float] = InputField(
+        default=0.7, description="Controls the randomness of predictions")
     repetition_penalty: Optional[float] = InputField(
         default=1.0,
         description="Penalty for repeated content in the generated text",
         ge=1,
         le=2,
     )
-    model_name: Optional[str] = InputField(default="gpt2", description="Favorite pretrained model to use")
-    favorate_models: Optional[
-        Literal[  # add your favorite models here
-            "",
-            "Meli/GPT2-Prompt",
-            "AUTOMATIC/promptgen-lexart",
-            "Gustavosta/MagicPrompt-Stable-Diffusion",
-            "succinctly/text2image-prompt-generator",
-            "MBZUAI/LaMini-Neo-1.3B",
-        ]
-    ] = InputField()
+    model_hf_name: Optional[str] = InputField(
+        default="gpt2", description="Hugging Face model name to use")
+    local_model: Optional[ModelLiteral] = InputField(
+        default="None", description="List of the local models to use from the model_cache folder"
+    )
 
     def is_sfw(self, text):
         banned_words = []  # add your banned words here eg. "nude", "murder"
@@ -63,18 +91,23 @@ class GPT2PromptInvocation(BaseInvocation):
         if trials > 5:
             return "\033[1;31mUnable to generate SFW prompt after 5 attempts.\033[0m"
 
-        model_to_use = self.model_name
-
-        if self.favorate_models and self.favorate_models != "":
-            model_to_use = self.favorate_models
+        # Determine which model to use
+        if self.local_model and self.local_model != "None":
+            model_to_use = os.path.join(CACHE_DIR, self.local_model)
+        else:
+            model_to_use = self.model_hf_name
 
         if not is_model_cached(model_to_use):
-            print(f"\033[1;32;40mDownloading model:   \033[0m \033[1;37;40m{model_to_use}\033[0m")
+            print(
+                f"\033[1;32;40mDownloading model:   \033[0m \033[1;37;40m{model_to_use}\033[0m")
         else:
-            print(f"\033[1;32;40mUsing cached model:   \033[0m \033[1;37;40m{model_to_use}\033[0m")
+            print(
+                f"\033[1;32;40mUsing cached model:   \033[0m \033[1;37;40m{model_to_use}\033[0m")
 
-        tokenizer = AutoTokenizer.from_pretrained(model_to_use, cache_dir=CACHE_DIR)
-        model = AutoModelForCausalLM.from_pretrained(model_to_use, cache_dir=CACHE_DIR)
+        tokenizer = AutoTokenizer.from_pretrained(
+            model_to_use, cache_dir=(None if self.local_model else CACHE_DIR))
+        model = AutoModelForCausalLM.from_pretrained(
+            model_to_use, cache_dir=(None if self.local_model else CACHE_DIR))
         device = "cuda" if torch.cuda.is_available() else "cpu"
         model = model.to(device)
 
@@ -83,8 +116,10 @@ class GPT2PromptInvocation(BaseInvocation):
         else:
             seed_with_context = seed
 
-        input_ids = tokenizer.encode(seed_with_context, return_tensors="pt").to(device)
-        attention_mask = torch.ones(input_ids.shape, dtype=torch.long, device=device)
+        input_ids = tokenizer.encode(
+            seed_with_context, return_tensors="pt").to(device)
+        attention_mask = torch.ones(
+            input_ids.shape, dtype=torch.long, device=device)
         pad_token_id = tokenizer.eos_token_id
 
         output = model.generate(
@@ -111,5 +146,6 @@ class GPT2PromptInvocation(BaseInvocation):
 
     def invoke(self, context: InvocationContext) -> StringOutput:
         prompt = self.generate_prompt(self.seed, self.context)
-        print(f"\033[1;32;40mGenerated Prompt:  \033[0m \033[1;37;40m{prompt}\033[0m")
+        print(
+            f"\033[1;32;40mGenerated Prompt:  \033[0m \033[1;37;40m{prompt}\033[0m")
         return StringOutput(value=prompt)
